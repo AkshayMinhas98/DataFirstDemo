8537a4a3380c04912a3ac219877c14a1df2ac60b
Akshay Minhas <Akshay.Minhas@rsystems.com>
2018-10-18 16:48:40 +0530

    Add project files.

---
+++ DataFirstDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DataFirstDemoContext();
+            var data = new DataFirst_Table()
+            {
+                PostID = 100,
+                DateofPublish = DateTime.Now,
+                Title = "When Air Become Breath",
+                Body = "asdadalhhivoirghhaoupejksfchfcpf"
+
+            };
+            context.DataFirst_Table.Add(data);
+            context.SaveChanges();
+            //Console.WriteLine($"Title = {context.DataFirst_Table}");
+            Console.ReadKey();
+        }
+    }
+}
